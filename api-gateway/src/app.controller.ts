7b92e434af85c1e2bdcbe46411d8b20211ffe0d6
Oscar Pérez <mr.omiguelperez@gmail.com>
2024-01-29 00:18:54 -0500

    publishing order to billing service and setup kafka

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateOrderRequest } from './create-order-request.input';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  async createOrder(@Body() createOrderRequest: CreateOrderRequest) {
+    return this.appService.createOrder(createOrderRequest);
   }
 }
--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './create-order-request.input';
+import { ClientKafka } from '@nestjs/microservices';
+import { MessageTypes } from './enums';
+import { OrderCreatedEvent } from './order-created.event';
+import * as uuid from 'uuid';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject('BILLING_SERVICE') private readonly billingClient: ClientKafka,
+  ) {}
+
+  createOrder(createOrderRequest: CreateOrderRequest) {
+    this.billingClient.emit(
+      MessageTypes.ORDER_CREATED,
+      new OrderCreatedEvent(
+        uuid.v4(),
+        createOrderRequest.userId,
+        createOrderRequest.price,
+      ),
+    );
   }
 }
