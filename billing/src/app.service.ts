fafd518b1e9c230e33f1a19eeae4215745c5b1d7
Oscar Pérez <mr.omiguelperez@gmail.com>
2024-01-29 08:35:51 -0500

    enhacements

--- billing/src/app.service.ts
+++ billing/src/app.service.ts
@@ -4,6 +4,6 @@ import { OrderCreatedEvent } from './order-created.event';
 @Injectable()
 export class AppService {
   async handleOrderCreatedEvent(data: OrderCreatedEvent) {
-    console.log('Service: Received event: ', data);
+    console.log(data);
   }
 }
